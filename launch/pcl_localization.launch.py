57a2b57c33e456d41539c482b1ffdcca082eb8b8
m11312045 <m11312045@gemail.yuntech.edu.tw>
2024-11-28 14:57:51 +0800

    TF改車體發布

--- launch/pcl_localization.launch.py
+++ launch/pcl_localization.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
         name='lidar_tf',
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0','0','0','0','0','0','1','base_link','velodyne']
+        arguments=['0.32','0','0.76','0','0','0','1','base_link','velodyne']
         )
 
     imu_tf = launch_ros.actions.Node(
@@ -90,7 +90,7 @@ def generate_launch_description():
     ld.add_action(from_inactive_to_active)
 
     ld.add_action(pcl_localization)
-    ld.add_action(lidar_tf)
+    # ld.add_action(lidar_tf)
     ld.add_action(to_inactive)
 
     return ld
